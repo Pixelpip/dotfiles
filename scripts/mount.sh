4e206b3460fc20a976471153e9304f21b0ae5948
PC480 <pc480@debian>
2024-01-29 23:54:40 +0530

    msg

--- scripts/mount.sh
+++ scripts/mount.sh
@@ -1,2 +1,1 @@
-mount -t cifs -o credentials=/home/desk480/scripts/.credentials //192.168.0.109/sambashare /mnt/sambashare
-mount UUID="7886DEAE86DE6C5E" /mnt/ssd
+mount -t cifs -o credentials=/home/pc480/dotfiles/scripts/.credentials //192.168.0.109/sambashare /mnt/sambashare
